53fc77dac4e3561d9cdc42f62da6e1240f0bfa3b
Francois Bertrand <francois.bertrand87@gmail.com>
2018-01-03 13:33:47 +0100

    defaultcontroller

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: burak
+ * Date: 12/20/17
+ * Time: 12:04 AM
+ */
+namespace App\Controller;
+use App\Repository\RunDiaryRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DefaultController extends Controller {
+
+    /*public function index(EntityManagerInterface $doctrine) {
+        //return ['users' => $doctrine->getRepository('App:User')->findAll()];
+    }*/
+
+    /**
+     * @Route("/", name="start")
+     * @Template("base.html.twig")
+     */
+    public function index(){
+
+    }
+}
