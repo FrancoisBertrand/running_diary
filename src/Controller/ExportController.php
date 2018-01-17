cf044920bcae4ae214b6c83ccd1395e33fb74073
Francois Bertrand <francois.bertrand87@gmail.com>
2018-01-17 14:14:55 +0100

    exporte zu einer funktion mit formatabfrage umprogrammiert

--- src/Controller/ExportController.php
+++ src/Controller/ExportController.php
@@ -30,51 +30,82 @@ class ExportController extends Controller{
      * @Route("/runners/{username}.{_format}", defaults={"_format":"json"}, name="runners")
      * @Method("GET")
      */
-    public function exportJSON(UserData $user, Request $request)
+    public function exportJSON(UserData $user, Request $request, $_format)
     {
-        $results = $this->getDoctrine()
-            ->getRepository(RunDiary::class)->findBy(['owner' => $user], ['date' => 'DESC']);
+        if($_format == "json"){
+            $results = $this->getDoctrine()
+                ->getRepository(RunDiary::class)->findBy(['owner' => $user], ['date' => 'DESC']);
 
-        $response = new Response();
-        $response->setContent(json_encode($results));
-        $response->headers->set('Content-Type', 'application/json');
+            $response = new Response();
+            $response->setContent(json_encode($results));
+            $response->headers->set('Content-Type', 'application/json');
 
-        return $response;
-    }
+            return $response;
+        }
+        else if($_format == 'csv'){
+            $resultsCSV = $this->getDoctrine()
+                ->getRepository(RunDiary::class)->findBy(['owner' => $user], ['date' => 'DESC']);
 
-
-    /**
-     * @Route("/runner/{username}.{_format}", defaults={"_format"="csv"}, name="runner")
-     * @Method("GET")
-     */
-    public function exportCSV(UserData $user, Request $request)
-    {
-        $resultsCSV = $this->getDoctrine()
-            ->getRepository(RunDiary::class)->findBy(['owner' => $user], ['date' => 'DESC']);
-
-        $responseCSV = new StreamedResponse();
-        $responseCSV->setCallback(
-            function () use ($resultsCSV) {
-                $handle = fopen('php://output', 'r+');
-                fputcsv($handle, array('User', 'strecke', 'dauer', 'durchschnitt'),';');
-                foreach ($resultsCSV as $row) {
-                    $data = array(
-                        $row->getOwner()->username,
-                        $row->getDistance(),
-                        $row->getDuration(),
-                        $row->getAvgSpeed(),
-                    );
-                    fputcsv($handle, $data);
+            $responseCSV = new StreamedResponse();
+            $responseCSV->setCallback(
+                function () use ($resultsCSV) {
+                    $handle = fopen('php://output', 'r+');
+                    fputcsv($handle, array('User', 'strecke', 'dauer', 'durchschnitt'),';');
+                    foreach ($resultsCSV as $row) {
+                        $data = array(
+                            $row->getOwner()->username,
+                            $row->getDistance(),
+                            $row->getDuration(),
+                            $row->getAvgSpeed(),
+                        );
+                        fputcsv($handle, $data);
+                    }
+                    fclose($handle);
                 }
-                fclose($handle);
+            );
+            $responseCSV->headers->set('Content-Disposition', sprintf('attachment; filename="%s"', $user->getUsername().'.csv'));
+            if (!$responseCSV->headers->has('Content-Type')) {
+                $responseCSV->headers->set('Content-Type', 'text/csv"');
             }
-        );
-        $responseCSV->headers->set('Content-Disposition', sprintf('attachment; filename="%s"', $user->getUsername().'.csv'));
-        if (!$responseCSV->headers->has('Content-Type')) {
-            $responseCSV->headers->set('Content-Type', 'text/csv"');
+
+            return $responseCSV;
         }
 
-        return $responseCSV;
     }
+
+
+//    /**
+//     * @Route("/runner/{username}.{_format}", defaults={"_format"="csv"}, name="runner")
+//     * @Method("GET")
+//     */
+//    public function exportCSV(UserData $user, Request $request)
+//    {
+//        $resultsCSV = $this->getDoctrine()
+//            ->getRepository(RunDiary::class)->findBy(['owner' => $user], ['date' => 'DESC']);
+//
+//        $responseCSV = new StreamedResponse();
+//        $responseCSV->setCallback(
+//            function () use ($resultsCSV) {
+//                $handle = fopen('php://output', 'r+');
+//                fputcsv($handle, array('User', 'strecke', 'dauer', 'durchschnitt'),';');
+//                foreach ($resultsCSV as $row) {
+//                    $data = array(
+//                        $row->getOwner()->username,
+//                        $row->getDistance(),
+//                        $row->getDuration(),
+//                        $row->getAvgSpeed(),
+//                    );
+//                    fputcsv($handle, $data);
+//                }
+//                fclose($handle);
+//            }
+//        );
+//        $responseCSV->headers->set('Content-Disposition', sprintf('attachment; filename="%s"', $user->getUsername().'.csv'));
+//        if (!$responseCSV->headers->has('Content-Type')) {
+//            $responseCSV->headers->set('Content-Type', 'text/csv"');
+//        }
+//
+//        return $responseCSV;
+//    }
 
 }
